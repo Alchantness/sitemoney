b88feeeee0316e99680456823bb437e6ffca825c
artemkons <konstantinov050701@gmail.com>
2020-03-17 22:52:19 +0300

    CSS

--- jsx/content.jsx
+++ jsx/content.jsx
@@ -1,13 +1,18 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 
+var style = {
+    borderWidth: "1px", 
+    borderColor: "red", 
+    borderStyle:"solid"
+}
 class Content extends React.Component{
     constructor(props){
         super(props)
 
     }
     render(){
-    return <h1>ДОБРЫЙ ВЕЧИР</h1>
+    return <div style = {style}>ДОБРЫЙ ВЕЧИР</div>
     }
 }
 
